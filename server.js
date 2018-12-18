6417f2c5ce98f4e15d15745049c37230c034872a
DeCryptinWeb <info@ivanvinas.ch>
2018-12-18 07:21:59 +0100

    Moved js in separate directory and removed index.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/dist/index.js', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.js')
+app.get('/dist/js/index.js', function (req, res) {
+    res.sendFile(__dirname + '/dist/js/index.js')
   })
 
   app.get('/index.map', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.map')
+    res.sendFile(__dirname + '/dist/js/index.map')
   })
 
 app.listen(3000, function () {
